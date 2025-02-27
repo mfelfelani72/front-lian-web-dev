c1d5ad1a465b144e9659dfee26afd44738287a5f
mohammad <mfelfelani72@gmail.com>
2025-02-27 10:22:02 +0000

    edit

--- src/features/welcome/WelcomeLanding.js
+++ src/features/welcome/WelcomeLanding.js
@@ -9,15 +9,15 @@ import TypoTextTitle from "../../features/core/components/TypoTextTitle.jsx"
 
 import trade_logo from "../../../assets/images/png/trade-logo.png"
 
-const WelcomeLanding=()=> {
+const WelcomeLanding = () => {
   return (
     <>
-     <div className="p-4 xs:w-[25rem] bg-[#FF3951] h-full">
+      <div className="p-4 xs:w-[25rem] bg-[#FF3951] h-full">
         <div className="flex flex-col gap-4 justify-center items-center h-full w-full">
-            <Image src={trade_logo} alt="trade-logo" className={"w-[250px] h-[180px]"} />
-            <TypoTextTitle className={"text-white text-3xl font-spaceGrotesk"} >Lian system</TypoTextTitle>
+          <Image src={trade_logo} alt="trade-logo" className={"w-[250px] h-[180px]"} />
+          <TypoTextTitle className={"text-white text-3xl font-spaceGrotesk"} >Lian system</TypoTextTitle>
         </div>
-     </div>
+      </div>
     </>
   )
 }
--- src/features/welcome/components/SlideScreen.jsx
+++ src/features/welcome/components/SlideScreen.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
+import React from "react";
 
 // Components
 
-import Image from "../../core/components/Image.jsx"
-import TypoTextTitle from '../../core/components/TypoTextTitle.jsx'
-import TypoTextInfo from '../../core/components/TypoTextInfo.jsx'
+import Image from "../../core/components/Image.jsx";
+import TypoTextTitle from "../../core/components/TypoTextTitle.jsx";
+import TypoTextInfo from "../../core/components/TypoTextInfo.jsx";
 
-// Svg
+// Svg & Png
 
-import slide_screen_1 from "../../../../assets/images/png/slide-screen-1.png"
+import slide_screen_1 from "../../../../assets/images/png/slide-screen-1.png";
+import arrow from "../../../../assets/images/svg/arrow-bg.svg";
 
 const SlideScreen = () => {
   return (
-   <>
-    <div className="p-4 xs:w-[25rem] h-full">
-      <div className="flex flex-col items-center justify-center h-full w-full">
-        <Image src={slide_screen_1} alt="slide_screen_1" className={"w-[300px] h-[180px]"} />
-        <TypoTextTitle className={"w-full text-left font-bold text-3xl mt-[100px]"}>Explore the world easily</TypoTextTitle>
-        <TypoTextInfo className={"w-full text-left mt-0.5 text-lg"}>To your desire</TypoTextInfo>
+    <>
+      <div className="flex flex-col w-full xs:w-[25rem] h-screen">
+        <div className="flex flex-col w-full h-[calc(60%)] justify-end items-center">
+          <Image
+            src={slide_screen_1}
+            alt="slide_screen_1"
+            className={"w-[300px] h-[180px]"}
+          />
+        </div>
+        <div className="flex flex-col w-full h-[calc(40%)] p-4 items-end justify-end">
+          <div className="">
+            <TypoTextTitle className={"w-full font-bold text-4xl"}>
+              Explore the world easily
+            </TypoTextTitle>
+          </div>
+          <div>
+            <TypoTextInfo className={"w-full mt-0.5 text-lg"}>
+              To your desire
+            </TypoTextInfo>
+          </div>
+          <div className="flex flex-row-reverse w-full justify-between mb-10">
+            <div className="flex flex-row-reverse items-center gap-[2px]">
+              <div className="w-6 h-2 bg-[#FF3951] rounded-2xl"></div>
+              <div className="w-4 h-2 bg-[#FFB6B6] rounded-2xl"></div>
+              <div className="w-4 h-2 bg-[#FFB6B6] rounded-2xl"></div>
+            </div>
+            {/* <div className="w-[50px] h-[50px] bg-[#252525] rounded-full"></div> */}
+            <Image
+            src={arrow}
+            alt="arrow"
+            className={"w-[50px] h-[50px]"}
+          />
+          </div>
+        </div>
       </div>
-    </div>
-   </>
-  )
-}
+    </>
+  );
+};
 
-export default SlideScreen
+export default SlideScreen;
