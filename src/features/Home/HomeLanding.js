d02d52fde6d89e0d4c0d6f1eae1fa0b8b2eb170e
mohammad <mfelfelani72@gmail.com>
2025-02-28 12:44:47 +0000

    edit

--- src/features/Home/HomeLanding.js
+++ src/features/Home/HomeLanding.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const HomeLanding = () => {
   return (
-    <div>HomeLanding</div>
-  )
-}
+    <div className="flex flex-col w-full xs:w-[25rem] h-full items-center justify-center bg-rose-400">
+      <div className="text-4xl cursor-pointer">login</div>
+      <div className="text-4xl cursor-pointer">register</div>
+    </div>
+  );
+};
 
-export default HomeLanding
+export default HomeLanding;
